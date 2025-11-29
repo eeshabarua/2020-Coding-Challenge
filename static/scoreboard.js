e3be1da9c06082d69715410d1b4e883dbc9fffd0
eeshabarua <eesha.barua@gmail.com>
2025-11-29 15:43:01 -0500

    sort scoreboard dynamically

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -6,6 +6,10 @@ function display_scoreboard(scoreboard) {
 }
 
 function renderScoreboard() {
+  // sort scoreboardState in descending order
+  scoreboardState.sort(function (a, b) {
+    return b.score - a.score;
+  });
   $("#teams").empty();
   $.each(scoreboard, function (index, team) {
     addTeamView(team.id, team.name, team.score);
